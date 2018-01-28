e3b6e288556548042bc94d8d1ebece654e79674c
Anusha <anu.july2@gmail.com>
2018-01-28 23:17:30 +1100

    Promises Part 1

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -8,16 +8,16 @@ export class PromotionService {
 
     constructor() { }
 
-    getPromotions(): Promotion[] {
-        return PROMOTION;
+    getPromotions(): Promise<Promotion[]> {
+        return Promise.resolve(PROMOTION);
     }
 
-    getPromotion(id: number): Promotion {
-        return PROMOTION.filter((promo) => (promo.id === id))[0];
+    getPromotion(id: number): Promise<Promotion> {
+        return Promise.resolve(PROMOTION.filter((promo) => (promo.id === id))[0]);
     }
 
-    getFeaturedPromotion(): Promotion {
-        return PROMOTION.filter((promo) => (promo.featured))[0];
+    getFeaturedPromotion(): Promise<Promotion> {
+        return Promise.resolve(PROMOTION.filter((promo) => (promo.featured))[0]);
     }
 
 }
